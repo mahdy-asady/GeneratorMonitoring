7f371c9e2738d67ba5f9a108f6c2be0993cb0c11
Mahdy Asady <mahdy.asady@gmail.com>
2022-12-11 10:35:45 +0330

    simple blink test

--- src/app/main.c
+++ src/app/main.c
@@ -1,10 +1,41 @@
+#include "stm32f1xx_hal.h"
 #include "clock.h"
 
 
+static void GPIO_Init(void);
+
 /* Main routine*/
 int main(void) {
+    HAL_Init();
     clockConfig();
 
+    GPIO_Init();
 
-    while(1);
+    while(1){
+        HAL_Delay(500);
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9);
+    }
+}
+
+
+static void GPIO_Init(void)
+{
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : PB9 */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
