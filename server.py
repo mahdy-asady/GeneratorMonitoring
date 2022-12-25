896b172fdfb971dccac47acb1aba1ab4afb9e448
Mahdy Asady <mahdy.asady@gmail.com>
2022-12-25 20:52:05 +0330

    Add console debug info

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
 import socket
 import struct
 
+#####################################################################################
+
 # UDP listening configs
 localIP     = "0.0.0.0"
 localPort   = 4000
 
 # Message buffer size
 bufferSize  = 36
+
+Debug = True
+
+#####################################################################################
+
 
 # Setup UDP listening
 def setupServer():
@@ -16,9 +23,44 @@ def setupServer():
     UDPServerSocket.bind((localIP, localPort))
     return UDPServerSocket
 
+def n2h(Number, Pad):
+    return hex(Number).center(Pad, " ")
+
+def debugPacket(Packet):
+    (PackVersion, PackMsgID, _1,
+     PackMCUID,
+     PackCRC,
+     PackVoltage, PackDuty, PackFreq,
+     PackTemp, PackHumid,
+     PackRPM, PackDigits, _2,
+     PackGyroX, PackGyroY,
+     PackGyroZ, PackAccX,
+     PackAccY, PackAccZ) = Packet
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|{}|{}|".format(n2h(PackVersion, 8), n2h(PackMsgID, 8), n2h(_1, 17)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|".format(n2h(PackMCUID, 35)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|".format(n2h(PackCRC, 35)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|{}|{}|".format(n2h(PackVoltage, 17), n2h(PackDuty, 8), n2h(PackFreq, 8)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|{}|".format(n2h(PackTemp, 17), n2h(PackHumid, 17)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|{}|{}|".format(n2h(PackRPM, 17), n2h(PackDigits, 8), n2h(_2, 8)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|{}|".format(n2h(PackGyroX, 17), n2h(PackGyroY, 17)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|{}|".format(n2h(PackGyroZ, 17), n2h(PackAccX, 17)))
+    print("\t\t+--------+--------+--------+--------+")
+    print("\t\t|{}|{}|".format(n2h(PackAccY, 17), n2h(PackAccZ, 17)))
+    print("\t\t+--------+--------+--------+--------+")
+    
+
 # Gets message, processes and stores to db and returns non-zero MessageID upon successfull operation
 def processMessage(Bytes):
     Packet = struct.unpack('<BBHLLHBBHHHBBHHHHHH', Bytes)
+    if Debug: debugPacket(Packet)
     # Extract fields
     (PackVersion, PackMsgID, _,
      PackMCUID,
@@ -40,15 +82,16 @@ def sendACK(Socket, ClientAddress, MessageID):
 
 
 Server = setupServer()
-print("UDP server up and listening...")
+print("UDP server up and listening...\n\n")
 
 # Listen for incoming datagrams
 while(True):
     BytesPair = Server.recvfrom(bufferSize)
     ClientAddress = BytesPair[1]
-
+    if Debug: print("Message Received from:", ClientAddress)
     MessageID = processMessage(BytesPair[0])
 
     if MessageID:
         # Sending a ACK to client
         sendACK(Server, ClientAddress, MessageID)
+    if Debug: print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
