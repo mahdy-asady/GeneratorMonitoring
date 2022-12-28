a4ca10bc344a04e7b7544ad22b63799447bf9c16
Mahdy Asady <mahdy.asady@gmail.com>
2022-12-28 23:53:03 +0330

    GPIO function moved to separate files in peripherals

--- src/app/main.c
+++ src/app/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "stm32f1xx_hal.h"
+#include "gpio.h"
 #include "clock.h"
 #include "usart.h"
 #include "esp/esp.h"
@@ -16,22 +17,20 @@ typedef struct {
     uint8_t Data;
 } ServerMessage;
 
-
-static void GPIO_Init(void);
-
 /* Main routine*/
 int main(void) {
     HAL_Init();
     clockConfig();
-
-    GPIO_Init();
+    gpioInit();
 
     usartHandle usartESP = {0},
                 usartDebug = {0};
+
     usartInit(&usartESP, USART1, 115200);
     usartInit(&usartDebug, USART2, 115200);
     usartEnableDebug(&usartDebug);
-    printf("Hello!!!\n");
+
+    printf("Board Start!!!\n");
 
     espInit(&usartESP, GPIOB, GPIO_PIN_8);
     espWifiConnect("esp32", "123456789");
@@ -48,42 +47,6 @@ int main(void) {
         if(usartRead(&usartESP, (uint8_t *)&ReceptionBuffer, 2, 500)) {
             printf("Message received: %d, %d\n", ReceptionBuffer.MessageType, ReceptionBuffer.Data);
         }
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9);
+        gpioToggleHealthLED();
     }
-}
-
-
-static void GPIO_Init(void)
-{
-
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-
-
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
-
-    /*Configure GPIO pin : PB9 */
-    GPIO_InitStruct.Pin = GPIO_PIN_9;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-
-    //Reset ESP Pin
-    /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
-
-    /*Configure GPIO pin : PB8 */
-    GPIO_InitStruct.Pin = GPIO_PIN_8;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-
 }
---
+++ src/peripherals/gpio.h
@@ -0,0 +1,8 @@
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+void gpioInit(void);
+
+void gpioToggleHealthLED(void);
+
+#endif
