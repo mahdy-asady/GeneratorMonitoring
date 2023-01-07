535cca76e7142782ea4542bf070e77844628c98c
Mahdy Asady <mahdy.asady@gmail.com>
2023-01-08 00:36:59 +0330

    32bit fifo implemented

--- src/shared/fifo/fifo.c
+++ src/shared/fifo/fifo.c
@@ -1,10 +1,12 @@
 #include "fifo.h"
 
+
+//*************** 8 bit fifo functions *********************************************************
 void fifo8Init(fifo8Data *fifo) {
     fifo->start = fifo->end = fifo->content;
 }
 
-uint8_t *_NextIndex(fifo8Data *fifo, uint8_t *Index) {
+uint8_t *_Next8bitIndex(fifo8Data *fifo, uint8_t *Index) {
     if((Index + 1) == (fifo->content + FIFO_BUFFER_SIZE))
         return fifo->content;
     else
@@ -13,12 +15,12 @@ uint8_t *_NextIndex(fifo8Data *fifo, uint8_t *Index) {
 
 void fifo8Push(fifo8Data *fifo, uint8_t Data) {
     *(fifo->end) = Data;
-    fifo->end = _NextIndex(fifo, fifo->end);
+    fifo->end = _Next8bitIndex(fifo, fifo->end);
 }
 
 uint8_t fifo8Pop(fifo8Data *fifo) {
     uint8_t Data = *(fifo->start);
-    fifo->start = _NextIndex(fifo, fifo->start);
+    fifo->start = _Next8bitIndex(fifo, fifo->start);
     return Data;
 }
 
@@ -28,5 +30,40 @@ bool fifo8IsEmpty(fifo8Data *fifo) {
 }
 
 bool fifo8IsFull(fifo8Data *fifo) {
-    return fifo->start == _NextIndex(fifo, fifo->end);
+    return fifo->start == _Next8bitIndex(fifo, fifo->end);
+}
+
+
+
+//*************** 32 bit fifo functions ********************************************************
+
+void fifo32Init(fifo32Data *fifo) {
+    fifo->start = fifo->end = fifo->content;
+}
+
+uint32_t *_Next32bitIndex(fifo32Data *fifo, uint32_t *Index) {
+    if((Index + 1) == (fifo->content + FIFO_BUFFER_SIZE))
+        return fifo->content;
+    else
+        return Index + 1;
+}
+
+void fifo32Push(fifo32Data *fifo, uint32_t Data) {
+    *(fifo->end) = Data;
+    fifo->end = _Next32bitIndex(fifo, fifo->end);
+}
+
+uint32_t fifo32Pop(fifo32Data *fifo) {
+    uint32_t Data = *(fifo->start);
+    fifo->start = _Next32bitIndex(fifo, fifo->start);
+    return Data;
+}
+
+
+bool fifo32IsEmpty(fifo32Data *fifo) {
+    return fifo->start == fifo->end;
+}
+
+bool fifo32IsFull(fifo32Data *fifo) {
+    return fifo->start == _Next32bitIndex(fifo, fifo->end);
 }
--- src/shared/fifo/fifo.h
+++ src/shared/fifo/fifo.h
@@ -12,6 +12,13 @@ typedef struct {
     uint8_t           *end;
 } fifo8Data;
 
+
+typedef struct {
+    uint32_t            content[FIFO_BUFFER_SIZE];
+    uint32_t           *start;
+    uint32_t           *end;
+} fifo32Data;
+
 void fifo8Init(fifo8Data *fifo);
 void fifo8Push(fifo8Data *fifo, uint8_t Data);
 uint8_t fifo8Pop(fifo8Data *fifo);
@@ -19,5 +26,12 @@ uint8_t fifo8Pop(fifo8Data *fifo);
 bool fifo8IsEmpty(fifo8Data *fifo);
 bool fifo8IsFull(fifo8Data *fifo);
 
+
+void fifo32Init(fifo32Data *fifo);
+void fifo32Push(fifo32Data *fifo, uint32_t Data);
+uint32_t fifo32Pop(fifo32Data *fifo);
+
+bool fifo32IsEmpty(fifo32Data *fifo);
+bool fifo32IsFull(fifo32Data *fifo);
 
 #endif
