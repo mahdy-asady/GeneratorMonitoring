869cf0cb0958a01e4628314888a3811baaa321be
Mahdy Asady <mahdy.asady@gmail.com>
2023-04-02 16:09:07 +0330

    sampling module implemented. timer still have some problems

--- src/shared/sampling/sampling.c
+++ src/shared/sampling/sampling.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+#include "sampling.h"
 #include "fifo/fifo.h"
 
-fifo32Data  frequencyFifo, dutyFifo, voltageFifo,
+extern fifo32Data  frequencyFifo, dutyFifo, voltageFifo,
 
             gyroXFifo,  gyroYFifo,  gyroZFifo,
             accelXFifo, accelYFifo, accelZFifo,
@@ -11,10 +11,41 @@ fifo32Data  frequencyFifo, dutyFifo, voltageFifo,
 
             rpmFifo;
 
-uint32_t samplingGetNormalizedValue(fifo32Data *Data) {
-    return 0;
+uint32_t getNormalizedValue(fifo32Data *Data) {
+    uint64_t sum = 0;
+    uint16_t count = 0;
+    while(!fifo32IsEmpty(Data)) {
+        count++;
+        sum += fifo32Pop(Data);
+    }
+    return sum / count;
 }
 
-void samplingGenerateReport(void) {
-    printf("Report\n");
+report samplingGenerateReport(void) {
+    report reportData = {0};
+    reportData.reportVersion = 1;
+
+
+    reportData.mcuID = 0;
+    reportData.powerVoltage = getNormalizedValue(&voltageFifo);
+    reportData.powerDuty = getNormalizedValue(&dutyFifo);
+    reportData.powerFrequency = getNormalizedValue(&frequencyFifo);
+    reportData.areaTemperature = getNormalizedValue(&tempFifo);
+    reportData.areaHumidity = getNormalizedValue(&humidFifo);
+    reportData.motorRPM = getNormalizedValue(&rpmFifo);
+    reportData.digital0 = 0;
+    reportData.digital1 = 0;
+    reportData.gyroX = getNormalizedValue(&gyroXFifo);
+    reportData.gyroY = getNormalizedValue(&gyroYFifo);
+    reportData.gyroZ = getNormalizedValue(&gyroZFifo);
+    reportData.accX = getNormalizedValue(&accelXFifo);
+    reportData.accY = getNormalizedValue(&accelYFifo);
+    reportData.accZ = getNormalizedValue(&accelZFifo);
+    reportData.checksum = 0;
+
+    printf("Report:\n");
+    printf("Voltage:%d\tDuty: %d\tFrequency:%d\n", reportData.powerVoltage, reportData.powerDuty, reportData.powerFrequency);
+    printf("Temp:%d\tHumid: %d\t\n", reportData.areaTemperature, reportData.areaHumidity);
+    printf("RPM:%d\tD0: %d\tD1:%d\n", reportData.motorRPM, reportData.digital0, reportData.digital1);
+    return reportData;
 }
--- src/shared/sampling/sampling.h
+++ src/shared/sampling/sampling.h
@@ -5,6 +5,27 @@
 
 #include "fifo/fifo.h"
 
-void samplingGenerateReport(void);
+typedef struct {
+    uint8_t reportVersion;
+    uint8_t packetID;
+    uint32_t mcuID;
+    uint16_t powerVoltage;
+    uint8_t powerDuty;
+    uint8_t powerFrequency;
+    uint16_t areaTemperature;
+    uint16_t areaHumidity;
+    uint16_t motorRPM;
+    bool digital0;
+    bool digital1;
+    uint16_t gyroX;
+    uint16_t gyroY;
+    uint16_t gyroZ;
+    uint16_t accX;
+    uint16_t accY;
+    uint16_t accZ;
+    uint32_t checksum;
+}report;
+
+report samplingGenerateReport(void);
 
 #endif
